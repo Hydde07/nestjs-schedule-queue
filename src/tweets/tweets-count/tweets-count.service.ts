fa91383346edecea11148051be7087c01f688cd5
Eddy Augusto Santos <guto_eddy@hotmail.com>
2022-10-06 21:32:09 -0300

    (init): First day of Nestjs studies

---
+++ src/tweets/tweets-count/tweets-count.service.ts
@@ -0,0 +1,32 @@
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
+import { Interval } from '@nestjs/schedule';
+import { InjectModel } from '@nestjs/sequelize';
+import { Cache } from 'cache-manager';
+import { Tweet } from '../entities/tweet.entity';
+
+@Injectable()
+export class TweetsCountService {
+    private limit = 10;
+    constructor(
+        @InjectModel(Tweet)
+        private tweetModel: typeof Tweet,
+        @Inject(CACHE_MANAGER)
+        private cacheManager: Cache,
+    ) {}
+
+    @Interval(1000)
+    async count() {
+        let offset = await this.cacheManager.get<number>('tweet-offset');
+        const tweets = await this.tweetModel.findAll({
+            offset: offset,
+            limit: this.limit,
+        });
+        console.log('counting');
+
+        if (tweets.length === this.limit) {
+            this.cacheManager.set('tweet-offset', offset + this.limit);
+            console.log(`achou mais de ${this.limit} tweets`);
+        }
+    }
+
+}
